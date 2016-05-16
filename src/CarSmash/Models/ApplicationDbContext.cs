3ae6f51900efdcc821410bb6e896670ae204fceb
lawton browning <lawton.a.browning@gmail.com>
2016-05-16 09:22:47 -0700

    add model

--- src/CarSmash/Models/ApplicationDbContext.cs
+++ src/CarSmash/Models/ApplicationDbContext.cs
@@ -9,8 +9,14 @@ namespace CarSmash.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
---
+++ src/CarSmash/Models/Image.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+using Microsoft.AspNet.Mvc;
+
+namespace CarSmash.Models
+{
+    [Table("Images")]
+    public class Image
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        public string Url { get; set; }
+        public virtual Product product { get; set; }
+
+    }
+}
