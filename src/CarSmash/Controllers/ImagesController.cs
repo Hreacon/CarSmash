bce3d323a68483000ffe2e613d16d31d4a4c27f8
lawtonB <lawton.a.browning@gmail.com>
2016-05-18 15:36:52 -0700

    fixed edit file upload

---
+++ src/CarSmash/Controllers/ImagesController.cs
@@ -0,0 +1,120 @@
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using CarSmash.Models;
+
+namespace CarSmash.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ImagesController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Images
+        public IActionResult Index()
+        {
+            return View(_context.Images.ToList());
+        }
+
+        // GET: Images/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Image image = _context.Images.Single(m => m.ImageId == id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(image);
+        }
+
+        // GET: Images/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Images/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Images.Add(image);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(image);
+        }
+
+        // GET: Images/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Image image = _context.Images.Single(m => m.ImageId == id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // POST: Images/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(image);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(image);
+        }
+
+        // GET: Images/Delete/5
+        [ActionName("Delete")]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Image image = _context.Images.Single(m => m.ImageId == id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(image);
+        }
+
+        // POST: Images/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Image image = _context.Images.Single(m => m.ImageId == id);
+            _context.Images.Remove(image);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
